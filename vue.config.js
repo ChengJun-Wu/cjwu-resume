004f4c2baf1682ce69afc5dec98395643fedd538
吴诚骏 <wuchengjun@wuchengjundeMacBook-Pro.local>
2022-02-17 11:44:21 +0800

    toPdf

---
+++ vue.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  chainWebpack: (config) => {
+    config
+      .plugin('html')
+      .tap((args) => {
+        args[0].title = '吴诚骏-五年经验-16619771137'
+        return args
+      })
+  }
+}
