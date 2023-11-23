a679efa2ec32e873e508c7606760bb80fc205bfa
吴诚骏 <wuchengjun@wuchengjundeMacBook-Pro.local>
2023-11-23 14:40:02 +0800

    修改年限

--- vue.config.js
+++ vue.config.js
@@ -3,7 +3,7 @@ module.exports = {
     config
       .plugin('html')
       .tap((args) => {
-        args[0].title = '吴诚骏-五年经验-16619771137'
+        args[0].title = '吴诚骏-七年经验-16619771137'
         return args
       })
   }
